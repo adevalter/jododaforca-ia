8a035b60020774dc7a99ac7e1418d5c00c4e805d
Adevalter <adevalter@hotmail.com>
2024-11-19 13:26:26 -0300

    update file

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String projectId = "tactile-oxygen-120814";
+        String projectId = ""; // sua id
         String location = "us-central1";
         String modelName = "gemini-1.5-flash-001";
 
